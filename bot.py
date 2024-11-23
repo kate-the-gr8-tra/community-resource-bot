fa7314f02a4125e6679f3713a30d78e5d2150154
Katie Tuttle <callmekatie21@outlook.com>
2024-11-23 05:37:03 -0500

    Added a database to track the servers in which the bot is active and which users have registered their info per server. Also ran some testing and got the bot to at least be online :3

--- bot.py
+++ bot.py
@@ -1,26 +1,26 @@
 import discord
 #from discord.ext import command
 import logging
-import discord.ext.commands.bot
-from emoji_list import EmojiList
+import discord.ext.commands.bot as bot
+from discord.ext import commands as ext_commands
 import asyncio
 from discord import app_commands
 from discord.app_commands import commands
 
 intents = discord.Intents.default()
-emoji_list = EmojiList()
+
 
 #~~~ TESTING SECTION ~~~
 with open('info.txt', "r") as file:
-    GUILD_IDS = file.read().splitlines()[0]
-    BOT_TOKEN = file.read().splitlines()[1]
+    info_list = file.read().splitlines()
+    GUILD_IDS = info_list[0]
+    BOT_TOKEN = info_list[1]
 #~~~ TESTING SECTION ~~~
 
 
-class ResourceBot(commands.Bot):
-    def __init__(self, bot):
+class ResourceBot(bot.Bot):
+    def __init__(self):
         super().__init__(command_prefix = '!', intents = intents)
-        self.bot = bot
         self.__hourly_phrase_toggle = False #will be for toggling the hourly phrase in a later function
 
     async def setup_hook(self):
@@ -36,6 +36,10 @@ class ResourceBot(commands.Bot):
     async def on_ready(self):
         print(f"'Logged in as {self.user}!'") 
 
+class MyCog(ext_commands.Cog):  
+    def __init__(self):
+        super().__init__()
+
     # Override on_message event
     async def on_message(self, message: discord.Message):
         if message.author.bot:
@@ -48,28 +52,28 @@ class ResourceBot(commands.Bot):
 
         for cop_phrase in cop_trigger_phrases:
             if cop_phrase in [word for word in message.content.split(" ").lower()]:
-                await message.channel.send(f"ACAB! {emoji_list.get(":police_officer:")} 
-                {emoji_list.get(":heavy_equals_sign:")} {emoji_list.get(":pig:")}")
+                await message.channel.send("ACAB! :police_officer:", \
+                ":heavy_equals_sign: :pig:") 
 
         if heat_from_fire in [word for word in message.content.split(" ").lower()]:
             await message.channel.send("Heat from fire! :3")
 
         for nazi_phrase in nazi_trigger_phrases:
             if nazi_phrase in [word for word in message.content.split(" ").lower()]:
-                await message.channel.send(f"MAKE THE WORLD A BETTER PLACE PUNCH A NAZI IN THE FACE! {emoji_list.get(":punch:")}")
+                await message.channel.send("MAKE THE WORLD A BETTER PLACE PUNCH A NAZI IN THE FACE! :punch:")
 
         if nazi_german_trigger in [word for word in message.content.split(" ").lower()]:
-            await message.channel.send(f"MACH DIE WELT EINEN BESSEREN ORT, SCHLAG EINEM NAZI INS GESICHT! {emoji_list.get(":punch:")}")
+            await message.channel.send("MACH DIE WELT EINEN BESSEREN ORT, SCHLAG EINEM NAZI INS GESICHT! :punch:")
 
     @app_commands.command(name="toggle_hourly_phrase", description="Toggles the bot's functionality to repeat the phrase 'Trans Rights' every hour")
     async def toggle_hourly_phrase(self, ctx: discord.ext.commands.Context):
         if not self.__hourly_phrase_toggle: #function will only do anything if the bot has not been already set to repeat the phrase
-            await bot.wait_until_ready() #wait for bot to be ready before starting a loop
+            await self.bot.wait_until_ready() #wait for bot to be ready before starting a loop
             channel_id = ctx.message.channel.id
-            channel = bot.get_channel(channel_id)
+            channel = self.bot.get_channel(channel_id)
 
-            while not bot.is_closed():
-                await ctx.send(f"{emoji_list.get(":transgender_symbol:")}Trans Rights!{emoji_list.get(":transgender_flag:")}") 
+            while not self.bot.is_closed():
+                await ctx.send(":transgender_symbol: Trans Rights! :transgender_flag:") 
                 self.__hourly_phrase_toggle = True #toggle the hourly phrase in case the command is called again
                 await asyncio.sleep(3600) #set the phrase to send every 1 hour (3600 seconds)
 
@@ -85,7 +89,7 @@ class ResourceBot(commands.Bot):
                        Trans Lifeline (US/Canada): https://translifeline.org \n'\
                        or LGBT Foundation (UK): https://lgbt.foundation for mental health support.")
         
-    
+    ''' To do:
     @app_commands.command(name="register", description="Register your pronouns and info")
     @app_commands.describe(link="Your pronouns.page link",
     name="Your preferred name",
@@ -93,21 +97,21 @@ class ResourceBot(commands.Bot):
     age="Your age (optional)")
     async def register(self, ctx: discord.ext.commands.Context):
         pass
-
+    '''
 
        
 
 async def main():
-    async with bot:
-        await bot.add_cog(ResourceBot(bot))
+    async with my_bot:
+        await my_bot.add_cog(MyCog())
         print("Cog added")
-        await bot.start(BOT_TOKEN)
+        await my_bot.start(BOT_TOKEN)
 
 
-bot = ResourceBot()
+my_bot = ResourceBot()
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 logging.basicConfig(level=logging.INFO, handlers=[handler])
 
-import asyncio
+
 asyncio.run(main())
 
